4ae7149854368a0e935ec9e0c6d2a2cb7684ae29
JakobEdvardsson <jakob@edvardsson.tech>
2025-10-30 14:27:17 +0100

    remove my basedpyright settings

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,35 +1,27 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      -- opts.servers.ruff = {
-      --   init_options = {
-      --     settings = {
-      --       lineLength = 100,
-      --     },
-      --   },
-      -- }
-
-      opts.servers.basedpyright = {
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              typeCheckingMode = "basic",
-            },
-            disableOrganizeImports = true,
-          },
-        },
-      }
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        python = { "ruff_fix", "ruff_format" },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     opts.servers.basedpyright = {
+  --       settings = {
+  --         basedpyright = {
+  --           analysis = {
+  --             autoSearchPaths = true,
+  --             useLibraryCodeForTypes = true,
+  --             typeCheckingMode = "strict",
+  --           },
+  --           disableOrganizeImports = true,
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       python = { "ruff_fix", "ruff_format" },
+  --     },
+  --   },
+  -- },
 }
