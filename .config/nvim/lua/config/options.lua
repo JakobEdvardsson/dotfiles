b4528e6e340a0c1d67e5a04a067f76b0efa646cf
JakobEdvardsson <jakob@edvardsson.tech>
2026-03-07 15:57:57 +0100

    remove cwd roop_spec

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,7 +5,6 @@
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.root_spec = { "cwd" }
 
 -- Disable eslint auto-format in UPX (type-aware eslint rules are heavy on save)
 if vim.fn.getcwd() == "/home/jakobe/code/upx" then
